94f95760d3b4fbc3b10da096d1c5fd287553c0c7
manujagobind <manujagobind@gmail.com>
2022-08-25 17:02:57 +0530

    Add a test for computing circle area

--- src/test/java/me/gobind/MathUtilsTest.java
+++ src/test/java/me/gobind/MathUtilsTest.java
@@ -7,12 +7,22 @@ import org.junit.jupiter.api.Test;
 public class MathUtilsTest {
     
     @Test
-    void test() {
+    void testAdd() {
         MathUtils mathUtils = new MathUtils();
 
         int expected = 2;
         int actual = mathUtils.add(1, 1);
 
-        assertEquals(expected, actual);
+        assertEquals(expected, actual, "This method should add two numbers.");
+    }
+
+    @Test
+    void testComputeCircleArea() {
+        MathUtils mathUtils = new MathUtils();
+
+        double expected = 314.1592653589793;
+        double actual = mathUtils.computeCircleArea(10);
+
+        assertEquals(expected, actual, "Should return circle area.");
     }
 }

7593ce38ff35f82acb8e8d89a2328f272bc2cf04
manujagobind <manujagobind@gmail.com>
2022-08-25 16:25:19 +0530

    Create a simple maven project and a basic test method

---
+++ src/test/java/me/gobind/MathUtilsTest.java
@@ -0,0 +1,13 @@
+package me.gobind;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MathUtilsTest {
+    
+    @Test
+    void test() {
+        System.out.println("This test ran!");
+    }
+}
